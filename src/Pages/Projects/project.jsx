8a952a13f059980a2de2cd852ea7feb103d671ca
Sourabh saini <sainisourabh927@gmail.com>
2025-06-18 11:38:41 +0530

    change

--- src/Pages/Projects/project.jsx
+++ src/Pages/Projects/project.jsx
@@ -3,11 +3,11 @@ import './project.css';
 import Project_card from '../Projects/project_card.jsx';
 
 function Project() {
-  
+
   const hader_pics = [
-    { id: 1, Image: '/project_img/IMG10.jpg', },
-    { id: 2, Image: '/project_img/IMG14.jpg', },
-    { id: 3, Image: '/project_img/IMG21.jpg', }
+    { id: 1, Image: '/project_img/IMG31.jpg', name: 'Modern Living Room' },
+    { id: 2, Image: '/project_img/IMG32.jpg', name: 'Luxury Bedroom' },
+    { id: 3, Image: '/project_img/IMG33.jpg', name: 'Classic Kitchen' }
   ];
 
   const sliderRef = useRef(null);
@@ -16,9 +16,9 @@ function Project() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCounter((prev) => (prev < hader_pics.length - 1 ? prev + 1 : 0));
-    }, 2500); // 1 second
+    }, 3000);
 
-    return () => clearInterval(interval); // cleanup on unmount
+    return () => clearInterval(interval);
   }, [hader_pics.length]);
 
   useEffect(() => {
@@ -29,23 +29,19 @@ function Project() {
 
   return (
     <>
-    <div className="project_container">
-      <div className="slider_main" ref={sliderRef}>
-
-        {hader_pics.map((slide) => (
-          <div key={slide.id} className="slider">
-            <img src={slide.Image} alt={slide.name} className="slider_pic" />
-          </div>
-        ))}
+      <div className="project_container">
+        <div className="slider_main" ref={sliderRef}>
+          {hader_pics.map((slide) => (
+            <div key={slide.id} className="slider">
+              <img src={slide.Image} alt={slide.name} className="slider_pic" />
+              <h1 className="slider_heading">{slide.name}</h1>
+            </div>
+          ))}
+        </div>
       </div>
-       
-    </div>
-    <Project_card/>
+      <Project_card />
     </>
-    
-  );
- 
-}
 
+  );}
 
 export default Project;
